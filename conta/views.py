e046b9bb3bfc6ac78ca13b563ed54077592339b2
gm-costa <gmaia_01@hotmail.com>
2023-07-07 06:10:00 -0300

    Cria view e template ver_contas

--- conta/views.py
+++ conta/views.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.urls import reverse
-from .models import ContaPagar
+from .models import ContaPaga, ContaPagar
 from perfil.models import Categoria
 
 
@@ -39,3 +40,26 @@ def definir_contas(request):
     else:
         categorias = Categoria.objects.all().order_by('nome')
         return render(request, 'definir_contas.html', {'categorias': categorias})
+
+
+def ver_contas(request):
+    MES_ATUAL = datetime.now().month
+    DIA_ATUAL = datetime.now().day
+    
+    contas = ContaPagar.objects.all()
+
+    contas_pagas = ContaPaga.objects.filter(data_pagamento__month=MES_ATUAL).values('conta')
+
+    contas_vencidas = contas.filter(dia_pagamento__lt=DIA_ATUAL).exclude(id__in=contas_pagas)
+    
+    contas_proximas_vencimento = contas.filter(dia_pagamento__lte = DIA_ATUAL + 5).filter(dia_pagamento__gte=DIA_ATUAL).exclude(id__in=contas_pagas)
+    
+    contas_restantes = contas.exclude(id__in=contas_vencidas).exclude(id__in=contas_pagas).exclude(id__in=contas_proximas_vencimento)
+
+    context = {
+        'contas_vencidas': contas_vencidas,
+        'contas_proximas_vencimento': contas_proximas_vencimento,
+        'contas_restantes': contas_restantes
+    }
+
+    return render(request, 'ver_contas.html', context)
